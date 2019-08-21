454bec45f200fb92923c7180017cb65b6640e82a
Craig White <craigwh10@gmail.com>
2019-08-21 12:55:02 +0100

    Conditional colouring added

--- src/components/Shoes/Shoe/Shoe.js
+++ src/components/Shoes/Shoe/Shoe.js
@@ -3,17 +3,33 @@ import uuidv4 from "uuid/v4";
 import "./Shoe.css";
 
 class Shoe extends Component {
+  classHandler = e => {
+    const classes = ["list-group-item"];
+
+    if (e < 2) {
+      classes.push("list-group-item-danger");
+      return [e, classes];
+    } else {
+      classes.push("list-group-item-success");
+      return [e, classes];
+    }
+  };
+
   render() {
     const items = this.props.sizes.map((item, index) => (
-      <fragment>
-        <li key={uuidv4()}>{item[0]}</li>
-        <li key={uuidv4()}>{item[1]}</li>
-      </fragment>
+      <ul key={uuidv4()} className="list-group list-group-horizontal">
+        <li className="list-group-item" key={uuidv4()}>
+          {item[0]}
+        </li>
+        <li className={this.classHandler(item[1])[1].join(" ")} key={uuidv4()}>
+          {this.classHandler(item[1])[0]}
+        </li>
+      </ul>
     ));
     return (
       <div className="col-lg-3">
         <h1>{this.props.name}</h1>
-        <ul>{items}</ul>
+        {items}
       </div>
     );
   }
--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -5,10 +5,30 @@ import Shoes from "../Shoes/Shoes";
 class App extends Component {
   state = {
     shoes: [
-      { id: "a1", name: "nike react", sizes: [[1, 1], [2, 1], [3, 2]] },
-      { id: "a2", name: "nike vaporfly", sizes: [[1, 1], [2, 1], [3, 2]] },
-      { id: "a3", name: "adidas ultraboost", sizes: [[1, 1], [2, 1], [3, 2]] },
-      { id: "a4", name: "adidas run", sizes: [[1, 1], [2, 4], [3, 8]] }
+      {
+        id: "a1",
+        name: "nike react",
+        sizes: [[1, 1], [2, 1], [3, 2]],
+        type: "run"
+      },
+      {
+        id: "a2",
+        name: "nike vaporfly",
+        sizes: [[1, 1], [2, 1], [3, 2]],
+        type: "sport"
+      },
+      {
+        id: "a3",
+        name: "adidas ultraboost",
+        sizes: [[1, 1], [2, 1], [3, 2]],
+        type: "sport"
+      },
+      {
+        id: "a4",
+        name: "adidas run",
+        sizes: [[1, 1], [2, 4], [3, 8]],
+        type: "run"
+      }
     ],
     filtered: false
   };
